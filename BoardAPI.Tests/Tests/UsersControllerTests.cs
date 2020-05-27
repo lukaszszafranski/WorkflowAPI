9fbfc7fc893b7cae58d55ec82c87bb72366dffae
lukaszszafranski <lukasz.szafranski16@wp.pl>
2020-05-27 17:14:27 +0200

    Added license to tests.

--- BoardAPI.Tests/Tests/UsersControllerTests.cs
+++ BoardAPI.Tests/Tests/UsersControllerTests.cs
@@ -1,4 +1,11 @@
-﻿using AutoMapper;
+﻿/* 
+ * Copyright (C) Work-FLow, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Łukasz Szafrański <lukasz.szafranski16@wp.pl>, Krzysztof Łepkowski, Szymon Lewandowski, May 2020
+ */
+
+using AutoMapper;
 using BoardAPI.Controllers;
 using BoardAPI.Helpers;
 using BoardAPI.Models.UserModels;
